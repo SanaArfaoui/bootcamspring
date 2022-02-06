903414fffaafc7bc968c0c29f0bb2c3bd2b82eb5
Arfaoui <sanaarfaoui@yahoo.fr>
2022-02-06 15:42:00 +0100

    tableau

--- src/main/java/com/sip/ams/controlers/HomeController.java
+++ src/main/java/com/sip/ams/controlers/HomeController.java
@@ -1,15 +1,30 @@
 package com.sip.ams.controlers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/info") //ce que  je tape dans l'url
-   public String info()
+   public String info(Model model)
+   
+   
    {
+		
+		String liste[] = new String[]{"OCP","oca","Spring","Angular"};
+		
+		ArrayList arlist = new ArrayList(Arrays.asList(liste));
+		
+		String formation ="fullstack";
 	System.out.println("méthode info");
+	model.addAttribute("workshop", formation);
+	
+	model.addAttribute("tableau",arlist);
 	   return "home/info" ; // le nom de la template
    }
 	@RequestMapping("/affichage")
